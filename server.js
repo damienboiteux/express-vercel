370ab2e5f47eb7403419ee6ce5f23c743bba2a51
damienboiteux.com <contact@damienboiteux.com>
2025-02-16 18:19:20 +0100

    Ajout cookie

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const test = require("./controllers/user");
+
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Subscribe to My channel");
-});
+app.get("/", test.login);
 
 app.listen(port, () => {
   `Server started on port ${port}`;
